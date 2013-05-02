ae904b61a9c3f28839306c4a086889398385e5ea
Louis T <LouisT@ltdev.im>
2013-05-01 21:38:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+NPA (v0.1.0)
+======
+
+Install: npm install npa
+
+This project is [Unlicensed](http://unlicense.org/ "Title").
+In other words, I don't care what you do with it.
+However, if you make something interesting, I would like to check it out.
+
+Information:
+------
+Well, the wiki article probably says enough. Find it [here](https://en.wikipedia.org/wiki/NATO_phonetic_alphabet "Title").
+
+    NOTE: This was for just for fun. Inspired by "Foxtrot Uniform Charlie Kilo" - Bloodhound Gang.
+
+Usage:
+------
+    var NPA = require('npa')();
+
+    NPA.toNPA(string) - Convert a string to NPA.
+
+    NPA.fromNPA(string) - Convert a string from NPA.
+
+    NPA.toMorse(string) - Convert a string to morse code.
+ 
+    NPA.fromMorse(string) - Convert a string from morse code.
+
+
+    Node.js:
+       var NPA = require('npa')(),
+           string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum.';
+       var morse = NPA.toMorse(string);
+       console.log('(toMorse) '+string+':\n '+morse+'\n\n');
+       console.log('(fromMorse) '+morse+':\n '+NPA.fromMorse(morse)+'\n\n');
+       var npa = NPA.toNPA(string);
+       console.log('(toNPA) '+string+':\n '+npa+'\n\n');
+       console.log('(fromNPA) '+npa+':\n '+NPA.fromNPA(npa)+'\n\n');
+
+    Browser:
+       <script src="./npa.js"></script>
+       <script>
+          var string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum.',
+              morse = NPA.toMorse(string);
+          alert('(toMorse) '+string+':\n '+morse+'\n\n');
+          alert('(fromMorse) '+morse+':\n '+NPA.fromMorse(morse)+'\n\n');
+          var npa = NPA.toNPA(string);
+          alert('(toNPA) '+string+':\n '+npa+'\n\n');
+          alert('(fromNPA) '+npa+':\n '+NPA.fromNPA(npa)+'\n\n');
+       </script>
---
+++ index.js
@@ -0,0 +1,116 @@
+/*
+  NPA (NATO phonetic alphabet) - v0.1.0 - Node.js & Browser
+  By Louis T. <louist@ltdev.im>
+  https://github.com/LouisT/node-npa/
+  https://en.wikipedia.org/wiki/NATO_phonetic_alphabet
+*/
+(function(Setup) {
+  "use strict"
+  var NPA = function () {
+          if (!(this instanceof NPA)) {
+             return new NPA();
+          };
+          this.data = {
+               'a':['Alfa','.-'],'b':['Bravo','-...'],'c':['Charlie','-.-.'],'d':['Delta','-..'],'e':['Echo','.'],
+               'f':['Foxtrot','..-.'],'g':['Golf','--.'],'h':['Hotel','....'],'i':['India','..'],'j':['Juliett','.---'],
+               'k':['Kilo','-.-'],'l':['Lima','.-..'],'m':['Mike','--'],'n':['November','-.'],'o':['Oscar','---'],
+               'p':['Papa','.--.'],'q':['Quebec','--.-'],'r':['Romeo','.-.'],'s':['Sierra','...'],'t':['Tango','-'],
+               'u':['Uniform','..-'],'v':['Victor','...-'],'w':['Whiskey','.--'],'x':['Xray','-..-'],'y':['Yankee','-.--'],
+               'z':['Zulu','--..'],'1':['One','.----'],'2':['Two','..---'],'3':['Three','...--'],'4':['Four','....-'],
+               '5':['Five','.....'],'6':['Six','-....'],'7':['Seven','--...'],'8':['Eight','---..'],'9':['Nine','----.'],
+               '0':['Zero','-----'],'-':['Dash','-....-'],',':['Comma','--..--'],
+          };
+          this.revs = {
+               'one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9','dash':'-',
+               'comma':',',
+          };
+  };
+  NPA.prototype.__ucfirst  = function (str) {
+          var sentences = String(str).match(/[^\s]([^\.\?\!]+)[^\s]/g),
+              fixes = sentences.map(function (sentence) {
+                     return sentence.replace(/^./gi,function(char) {
+                            return char.toUpperCase();
+                     });
+              });
+          for (var fix in fixes) {
+              str = str.replace(sentences[fix],fixes[fix]);
+          };
+          return str;
+  };
+  NPA.prototype.__buildMorse = function () {
+          if (!('morse' in this)) {
+             this.morse = {};
+             for (var key in this.data) {
+                 if (this.data.hasOwnProperty(key)) {
+                    this.morse[this.data[key][1]] = key;
+                 };
+             };
+          };
+          return (!!Object.keys(this.morse).length?this.morse:false);
+  };
+  NPA.prototype.__runner = function (str,mode,sep) {
+          var str = String(str).toLowerCase(),
+              sep = sep||'-';
+          switch (mode) {
+                 case 2:
+                      var spaces = str.split(sep),
+                          ret = [];
+                      for (var num in spaces) {
+                          var piece = [], 
+                              split = spaces[num].split(',');
+                          for (var letn in split) {
+                              var curr = split[letn].toLowerCase();
+                              piece.push((!(curr in this.revs)?curr[0]:this.revs[curr]));
+                          };
+                          ret.push(piece.join(''));
+                      };
+                      return this.__ucfirst(ret.join(' '));
+                 case 3:
+                      if (this.__buildMorse()) {
+                         var spaces = str.replace(/[^\-\.\s]/g,'').split('  '),
+                             ret = [];
+                         for (var num in spaces) {
+                             var piece = [],
+                                 split = spaces[num].split(' ');
+                             for (var morn in split) {
+                                 if (this.morse[split[morn]]) {
+                                    piece.push(this.morse[split[morn]]);
+                                 };
+                             };
+                             ret.push(piece.join(''));
+                         };
+                         return ret.join(' ').toUpperCase();
+                      };
+                      return false;
+                 case 1:
+                 default:
+                      var spaces = str.split(' '),
+                          ret = [];
+                      for (var num in spaces) {
+                          var piece = [],
+                              self = this;
+                          if (mode === 1) {
+                             spaces[num] = spaces[num].replace(/[^a-z0-9\s\-]/gi,'');
+                          };
+                          spaces[num].replace(/./gi,function (char) {
+                              piece.push((self.data[char]?self.data[char][(mode===1?'1':'0')]:char));
+                          });
+                          ret.push(piece.join((mode===1?' ':',')));
+                      };
+                      return ret.join((mode===1?'  ':sep));
+          };
+  };
+  NPA.prototype.toMorse = function (str) {
+          return this.__runner(str,1);
+  };
+  NPA.prototype.fromMorse = function (str) {
+          return this.__runner(str,3);
+  };
+  NPA.prototype.toNPA = function (str,sep) {
+          return this.__runner(str,0,sep);
+  };
+  NPA.prototype.fromNPA = function (str,sep) {
+          return this.__runner(str,2,sep);
+  };
+  Setup(NPA);
+})((typeof exports!=='undefined'?function(fn){module.exports=fn;}:function(fn){this['NPA']=fn();}));
